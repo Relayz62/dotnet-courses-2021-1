84ee8f5a3d723f9ce490650690bdfbe81c26e3cc
Relayz62 <Relayz62@gmail.com>
2021-03-19 00:11:01 +0300

    Complete

---
+++ 07-generics/Generics/Task1/DynamicArray.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    class DynamicArray<T> where T : new()
+    {
+        private T[] _itemsArray;
+        private int _size;
+        private static readonly T[] _emptyArray = new T[0];
+        private static readonly int _defaultCapacity = 8;
+
+        public DynamicArray()
+        {
+            Length = 0;
+            _itemsArray = new T[_defaultCapacity];
+        }
+
+        public DynamicArray(int capacity)
+        {
+            if (_defaultCapacity < 0) throw new ArgumentOutOfRangeException();
+            if (_defaultCapacity == 0) _itemsArray = _emptyArray;
+            else _itemsArray = new T[capacity];
+        }
+
+        public DynamicArray(IEnumerable<T> collection)
+        {
+            if (collection == null) throw new ArgumentNullException();
+
+            ICollection<T> listCollection = collection as ICollection<T>;
+            if (listCollection != null)
+            {
+                int count = listCollection.Count;
+                if (count == 0)
+                {
+                    _itemsArray = _emptyArray;
+                }
+                else
+                {
+                    _itemsArray = new T[count];
+                    listCollection.CopyTo(_itemsArray, 0);
+                    Length = count;
+                }
+            }
+            else
+            {
+                Length = 0;
+                _itemsArray = _emptyArray;
+            }
+        }
+
+        public DynamicArray(T[] array)
+        {
+            if(array == null)
+            {
+                _itemsArray = _emptyArray;
+                Length = 0;
+            }
+            else
+            {
+                Length = array.Length;
+                _itemsArray = new T[Length];
+                array.CopyTo(_itemsArray, 0);
+            }
+        }
+
+        public int Length
+        {
+            get { return _size; }
+            private set
+            {
+                _size = value;
+            }
+        }
+        public int Capacity
+        {
+            get { return _itemsArray.Length; }
+            set
+            {
+                if (value < Length)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                if (value != Length)
+                {
+                    if (value > 0)
+                    {
+                        T[] newItems = new T[value];
+                        if (Length > 0)
+                        {
+                            Array.Copy(_itemsArray, 0, newItems, 0, Length);
+                        }
+                        _itemsArray = newItems;
+                    }
+                    else
+                    {
+                        _itemsArray = _emptyArray;
+                    }
+                }
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= Length) throw new ArgumentOutOfRangeException();
+                else return _itemsArray[index];
+            }
+            set
+            {
+                if (index >= Length) throw new ArgumentOutOfRangeException();
+                else _itemsArray[index] = value;
+            }
+        }
+
+        public void Add(T value)
+        {
+            if(Length == _itemsArray.Length)
+            {
+                Array.Resize(ref _itemsArray, _itemsArray.Length * 2);
+            }
+            _itemsArray[Length++] = value;
+        }
+
+        public void AddRange(params T[] values)
+        {
+            if(_itemsArray.Length < values.Length + Length)
+            {
+                Array.Resize(ref _itemsArray, _itemsArray.Length + values.Length);
+            }
+            values.CopyTo(_itemsArray, Length);
+            Length += values.Length;
+        }
+
+        public bool Remove(T value)
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                if(Equals(_itemsArray[i],value))
+                {
+                    RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void RemoveAt(int index)
+        {
+            if (index >= Length)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            Length--;
+            if (index < Length)
+            {
+                Array.Copy(_itemsArray, index + 1, _itemsArray, index, _size - index);
+            }
+            _itemsArray[Length] = default(T);
+        }
+
+        public void Insert(int index, T value)
+        {
+            if (index > Capacity)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (Capacity == _itemsArray.Length) Capacity++;
+            if (index < Capacity)
+            {
+                Array.Copy(_itemsArray, index, _itemsArray, index + 1, Length - index);
+            }
+            _itemsArray[index] = value;
+            Length++;
+        }
+
+
+        public void PrintDynamicArray()
+        {
+            Console.WriteLine("/////////////////////////////////////");
+            for (int i = 0; i < Length; i++)
+            {
+                Console.WriteLine(_itemsArray[i]);
+            }
+            Console.WriteLine($"Capacity: {Capacity}");
+            Console.WriteLine($"Length: {Length}");
+        }
+    }
+}
---
+++ 07-generics/Generics/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] mass1 = new int[5] { 1, 2, 3, 4, 5 };
+            List<int> list = new List<int>() { 1, 2, 3 };
+            DynamicArray<int> test1 = new DynamicArray<int>(list);
+            test1.PrintDynamicArray();
+            test1.Add(2);
+            test1.PrintDynamicArray();
+            test1.AddRange(mass1);
+            test1.PrintDynamicArray();
+            test1.Insert(0, 22);
+            test1.PrintDynamicArray();
+            test1.Insert(4, 22);
+            test1.PrintDynamicArray();
+            test1.Remove(22);
+            test1.PrintDynamicArray();           
+            test1.Remove(22345);
+
+        }
+    }
+}
